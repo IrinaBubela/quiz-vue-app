dd9b47066d0d9e8f97cc2ee9cb72d105c7d241e7
Irina Bubela <Iryna.Bubela@epam.com>
2024-08-19 00:27:25 +0200

    Updates

--- vue.config.js
+++
@@ -1,9 +0,0 @@
-const { defineConfig } = require('@vue/cli-service')
-
-module.exports = defineConfig({
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/quiz-vue-app/'
-    : '/',
-  transpileDependencies: true,
-  runtimeCompiler: true,
-});
